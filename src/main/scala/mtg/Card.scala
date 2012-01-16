77b5375d019ec5775f0149d23142016b13d112fc
Alexey Romanchuk <alexey.romanchuk@gmail.com>
2012-01-16 23:01:39 +0600

    SSG: Cleanup

--- src/main/scala/mtg/Card.scala
+++ src/main/scala/mtg/Card.scala
@@ -2,7 +2,6 @@ package mtg
 
 import java.net.URL
 
-import scala.io.Source
 import scala.xml._
 import scala.xml.parsing._
 import java.io.InputStream
@@ -12,18 +11,17 @@ case class Card(name: String, condition: String, price: Double)
 case class SSGPageInfo(cards: Seq[Card], hasNext: Boolean)
 
 case class SSGPage(edition: String, offset: Int = 0) {
-  lazy val pageInfo: SSGPageInfo = {
+  private lazy val pageInfo: SSGPageInfo = {
     val url = new URL("http://sales.starcitygames.com//spoiler/display.php?" +
       "s%5Bcor2%5D=" + edition +
       "&%s&display=4" +
       "&startnum=" + offset +
       "&numpage=200")
-
-    //    val input = Source.fromURL(url)
-    //    val pageContent = input.getLines().foldLeft("")((A, B) => A + "\n" + B)
     SSGPageParser.parse(url.openConnection().getInputStream)
   }
 
+  def getCards = pageInfo.cards
+  def isHasNext = pageInfo.hasNext
 
 }
 
@@ -31,12 +29,7 @@ class SSGPageSearch(edition: String) extends Iterator[SSGPage] {
   var currentOffset: Int = 0
   var currentPage: SSGPage = null
 
-  def hasNext = {
-    if (currentPage == null)
-      true
-    else
-      currentPage.pageInfo.hasNext
-  }
+  def hasNext = {(currentPage == null || currentPage.isHasNext)}
 
   def next() = {
     if (currentPage != null)
@@ -49,22 +42,17 @@ class SSGPageSearch(edition: String) extends Iterator[SSGPage] {
 
 object SSGPageParser {
   def parse(is: InputStream): SSGPageInfo = {
-
     val html = HTMLParser.load(is)
-
     val tr = html \\ "tr"
     val cardLines = tr.slice(2, tr.length - 4)
 
     var cardName: String = null;
-
     val cards = cardLines map {
       W =>
         val cells = (W \\ "td")
-
         val currentText = cells(0).text.trim
         if (currentText.length() > 3) cardName = currentText
         val condition = cells(cells.length - 4).text.trim
-
         new Card(cardName, condition, cells(cells.length - 2).text.trim.substring(1).toDouble)
     }
 
@@ -86,13 +74,12 @@ object SSGPageParser {
       rootElem
     }
   }
-
 }
 
-object Test extends Application {
+object Test extends App {
   var result: List[Card] = List[Card]()
   for (p <- new SSGPageSearch("1001")) {
-    result = result :::(p.pageInfo.cards.toList)
+    result = result :::(p.getCards.toList)
   }
   println(result)
 }
