934f31a5bce186f8e20200a85a21c52ce62c9e8f
Alexey Romanchuk <romanchuk-as@ess54.ru>
2012-01-30 17:00:11 +0700

    JSON API

--- src/main/java/mtg/rest/CardResource.scala
+++ src/main/java/mtg/rest/CardResource.scala
@@ -3,33 +3,39 @@ package mtg.rest
 import javax.ws.rs.{QueryParam, GET, Path}
 import mtg.persistance.Connection
 import com.mongodb.casbah.commons.MongoDBObject
-import org.codehaus.jettison.json.{JSONObject, JSONArray}
 import collection.JavaConversions
+import org.codehaus.jettison.json.{JSONArray, JSONObject}
 
 
 @Path("/price")
 class CardResource extends Connection {
+
+  private val max_size : Int = 50;
+
   @GET
-  def searchCard(@QueryParam("name") name: String) = {
+  def searchCard(
+    @QueryParam("name") name: String,
+    @QueryParam("offset") offset: Int,
+    @QueryParam("offset") size: Int
+  ) = {
+    val maxSize = if(size > 50)  50 else size;
     val builder = MongoDBObject.newBuilder
     if (name != null) builder += "card_name" -> name
 
-
-    val result: JSONArray = new JSONArray()
     connection("price")
-    .find(builder.result)
-    .map( obj => {
-      val result = new JSONObject
-      for(x <- new JavaConversions.JSetWrapper(obj.keySet())) result.put(x, obj.get(x))
-      result
-    })
-    .foreach(result.put(_))
-    result
+      .find(builder.result)
+      .limit(maxSize)
+      .skip(offset)
+      .map(obj => {
+        new JavaConversions.JSetWrapper(obj.keySet())
+          .foldLeft(new JSONObject)((array, item) => array.put(item, obj.get(item)))
+      })
+      .foldLeft(new JSONArray)((array, item) => array.put(item))
   }
 }
 
 object JSONSerializer {
-  def serialize(input : AnyVal) {
+  def serialize(input: AnyVal) {
 
   }
 }
