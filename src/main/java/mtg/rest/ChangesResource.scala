b290f66dd4ca9905b45a7becbc3188ceb05682a8
Alexey Romanchuk <alexey.romanchuk@gmail.com>
2012-02-26 18:49:15 +0600

    Sort order

--- src/main/java/mtg/rest/ChangesResource.scala
+++ src/main/java/mtg/rest/ChangesResource.scala
@@ -34,7 +34,7 @@ class ChangesResource extends Connection with Logging {
 
     val result = conn("price2")
       .find(PriceSnapShotMapping.date >= begin < end && PriceSnapShotMapping.absDiff > 0)
-      .sort("absDiff".fieldOf[Double](-1))
+      .sort("absDiff".fieldOf[Double](-1) ~ "item.name".fieldOf[Double](1) ~ "item.edition".fieldOf[Double](1))
       .limit(maxSize)
       .skip(offset)
       .map(JSONTransformer.transform(_))
