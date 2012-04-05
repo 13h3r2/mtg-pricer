1b1ca12f497acc532db62e637670ad2d9e28435d
Alexey Romanchuk <romanchuk-as@ess54.ru>
2012-04-05 17:24:36 +0700

    Track update process

--- src/main/java/mtg/actions/PriceUpdater.scala
+++ src/main/java/mtg/actions/PriceUpdater.scala
@@ -3,20 +3,16 @@ package mtg.actions
 import mtg.persistence.CardDAO
 import actors.scheduler.ForkJoinScheduler
 import com.weiglewilczek.slf4s.Logging
-import mtg.model.PriceSnapshot
 import mtg.ssg.SSGPriceProvider
 import mtg.persistence.{PriceUpdateActionDAO, EditionDAO}
-import mtg.model.PriceUpdateAction
 import java.util.Date
 import actors.{IScheduler, Actor}
 import mtg.actions.PriceUpdater.PrintActorScheduler
+import mtg.model.{Edition, PriceSnapshot, PriceUpdateAction}
 
 object PriceUpdateCommand {
   def doIt() = {
-    val sch = PrintActorScheduler.newScheduler()
-    EditionDAO.findAll(10000).foreach(e => {
-      PriceUpdater.updatePrice(new SSGPriceProvider(e), sch)
-    })
+    PriceUpdater.update(EditionDAO.findAll(10000))
     PriceUpdateActionDAO.insert(new PriceUpdateAction(new Date()))
   }
 }
@@ -28,13 +24,18 @@ trait PriceProvider {
 
 object PriceUpdater extends Logging {
 
+  def update(provider: Iterator[Edition]) {
+    val sch = PrintActorScheduler.newScheduler()
+    provider.foreach(a => PriceUpdater.updatePrice(new SSGPriceProvider(a), sch))
+  }
+
   def updatePrice(provider: PriceProvider, sch: IScheduler) {
     val actor = new UpdateActor(sch)
     actor.start()
     actor ! provider
   }
 
-  case class UpdateActor(sch: IScheduler) extends Actor {
+  class UpdateActor(sch: IScheduler) extends Actor {
 
     override def scheduler = sch
 
@@ -51,12 +52,13 @@ object PriceUpdater extends Logging {
     }
   }
 
-  object PrintActorScheduler  {
-     def newScheduler() : IScheduler = {
+  object PrintActorScheduler {
+    def newScheduler(): IScheduler = {
       logger.debug("create scheduler")
       val s = new ForkJoinScheduler(10, 10, true, false)
       s.start()
       s
     }
   }
+
 }
