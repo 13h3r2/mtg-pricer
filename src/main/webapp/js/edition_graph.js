77cbbf6d11b194bcff35d5ede64774359eb8068c
Alexey Romanchuk <alexey.romanchuk@gmail.com>
2013-05-07 11:31:15 +0700

    Edition price changes support

--- src/main/webapp/js/edition_graph.js
+++ src/main/webapp/js/edition_graph.js
@@ -4,7 +4,6 @@ function EditionGraph(page) {
         document.getElementById("edition-graph"),
         "Date,Temperature\n" +
             "2008-05-07,75\n" +
-            "2008-05-08,70\n" +
             "2008-05-09,80\n",
         {"includeZero": true, "width": 900, "height": 450}
     );
